6de51733829eb1f7e0fc57469ed059c25b8e4f9c
Deathstalker <iamsoumyass@gmail.com>
2022-03-19 18:57:05 +0530

    started bulding amazon clone

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header'
+import Checkout from './components/Checkout'
+import Home from './components/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Header.css'
+// import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { SearchIcon } from '@chakra-ui/icons'
+import { Link } from 'react-router-dom'
+import { useStateValue } from './StateProvider'
+
+const Header = () => {
+  const [{ basket }, dispatch] = useStateValue()
+
+
+  return (
+    <div className='header'>
+      <Link to='/'>
+        <img
+          className='header-logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        />
+      </Link>
+
+      <div className='header-search'>
+        <input
+          type='text'
+          className='header-search-input'
+        />
+        <SearchIcon className='header-search-icon' />
+      </div>
+
+      <div className='header-nav'>
+        <div className='header-option'>
+          <span className='header-optionLine1'>Hello Guest</span>
+          <span className='header-optionLine2'>Sign in</span>
+        </div>
+        <div className='header-option'>
+          <span className='header-optionLine1'>Returns</span>
+          <span className='header-optionLine2'>Orders</span>
+        </div>
+        <div className='header-option'>
+          <span className='header-optionLine1'>Your</span>
+          <span className='header-optionLine2'>Prime</span>
+        </div>
+
+        {/* <Link to = '/checkout'>
+          <div className='header-option-basket'>
+            <ShoppingBasket />
+            <span className = 'header-optionLine2 header-basketCount'>{basket?.length}</span>
+          </div>
+        </Link> */}
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
